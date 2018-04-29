7b6465fbf160faf6e19c0cf458eaa3b89fa850c3
unknown <JINJUN SU@PROD.HCLPNP.COM>
2018-04-29 11:03:06 +0800

    update

--- testNumpy.py
+++ testNumpy.py
@@ -1,6 +1,30 @@
 
 import numpy as np
 
+a= np.array([1,2,3,4,5,6,8,9,0])
+print(a)
+
+b = np.full((100,300), 7)
+print(b)
+
+c= np.eye(2)
+print(c)
+
+
+a = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+
+# Use slicing to pull out the subarray consisting of the first 2 rows
+# and columns 1 and 2; b is the following array of shape (2, 2):
+# [[2 3]
+#  [6 7]]
+b = a[:3, 1:]
+
+print(a[0,0],a[2,2])
+print(a)
+print(b)
+
+
+
 a = np.array([1, 2, 3])   # Create a rank 1 array
 print(type(a))            # Prints "<class 'numpy.ndarray'>"
 print(a.shape)            # Prints "(3,)"
@@ -11,3 +35,19 @@ print(a)                  # Prints "[5, 2, 3]"
 b = np.array([[1,2,3],[4,5,6]])    # Create a rank 2 array
 print(b.shape)                     # Prints "(2, 3)"
 print(b[0, 0], b[0, 1], b[1, 0])   # Prints "1 2 4"
+a = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+
+# Two ways of accessing the data in the middle row of the array.
+# Mixing integer indexing with slices yields an array of lower rank,
+# while using only slices yields an array of the same rank as the
+# original array:
+row_r1 = a[1, :]    # Rank 1 view of the second row of a
+row_r2 = a[1:2, :]  # Rank 2 view of the second row of a
+print(row_r1, row_r1.shape)  # Prints "[5 6 7 8] (4,)"
+print(row_r2, row_r2.shape)  # Prints "[[5 6 7 8]] (1, 4)"
+
+# We can make the same distinction when accessing columns of an array:
+col_r1 = a[:, 1]
+col_r2 = a[:, 1:2]
+print(col_r1, col_r1.shape)  # Prints "[ 2  6 10] (3,)"
+print(col_r2, col_r2.shape)  # Prints "[[ 2]
--- testpy.py
+++ testpy.py
@@ -10,5 +10,79 @@ checkSum('sujj',True)
 
 
 class Util(object):
-    def __init__(self,method=str)：
+    def __init__(self,method="check"):
         self.method = method
+
+
+
+print(Util().__str__)
+#lists
+a1=[1,2,3,4,5,6,7,8]
+nums = list(range(5)) 
+print(a1[0:-1])
+print(nums.__len__())
+print(nums[:])
+animals=['lion','cat','fish','horse']
+for i in animals:
+    print(i)
+animals = ['cat', 'dog', 'monkey']
+
+#maybe the interceptor implements the codes as key,value pair or turple
+for idx, animal in enumerate(animals):
+    print('#%d: %s' % (idx + 1, animal))
+
+
+nums = [0, 1, 2, 3, 4]
+squares = []
+for x in nums:
+    squares.append(x ** 2)
+print(squares)   # Prints [0, 1, 4, 9, 16]
+
+
+nums = [0, 1, 2, 3, 4]
+squares = [x ** 2 for x in nums]
+print(squares)   # Prints [0, 1, 4, 9, 16]
+
+numbers=[0,1,2,3,4,5,6,7,8,9]
+squrt = [x**2 for x in numbers]
+print (squrt)
+
+
+words1 =['I',"He","She"]
+words2 = ['love','eat','run','hit','chase']
+words3=['food','flower','butterfly','fly','pig','glass']
+#{(x,y,z):x for x in words1,y for y in words2,z for z in words3} 
+#still list can also support the condition to create it.
+#[(x, y) for x in [1,2,3] for y in [3,1,4] if x != y]
+sentence =[( x, y ,z) for x in words1 for y in words2 for z in words3]
+#print(sentence)
+for s in sentence:
+    print(s[0]+" "+s[1]+" "+s[2])
+#dictionary
+d = {'cat': 'cute', 'dog': 'furry'} 
+
+d['dog']='food'
+print(d['cat'])
+print(d['dog'])
+
+d = {'person': 2, 'cat': 4, 'spider': 8}
+for animal in d:
+    legs = d[animal]
+    print('A %s has %d legs' % (animal, legs))
+
+
+#sets
+animals = {'cat', 'dog'}
+print('cat' in animals)   # Check if an element is in a set; prints "True"
+print('fish' in animals)  # prints "False"
+animals.add('fish')       # Add an element to a set
+print('fish' in animals)  # Prints "True"
+print(len(animals))       # Number of elements in a set; prints "3"
+animals.add('cat')        # Adding an element that is already in the set does nothing
+print(len(animals))       # Prints "3"
+animals.remove('cat')     # Remove an element from a set
+print(len(animals))       # Prints "2"
+
+
+#Tuples
+d = {(x, x + 1): x for x in range(10)}  
