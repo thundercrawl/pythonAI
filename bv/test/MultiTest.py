1c29ed33b2aebad247ad1a3ae82a9e1690e3816a
thundercrawl <brightpegion@gmail.com>
2019-04-15 10:15:39 +0800

    update

--- bv/test/MultiTest.py
+++ bv/test/MultiTest.py
@@ -6,6 +6,10 @@ exitFlag = 0
 interval = 1
 maxThreads=1
 maxloop=1000
+g_count=0
+g_next=0
+
+g_lock = threading.RLock()
 class WorkerThread (threading.Thread):
     def __init__(self, threadID, name, counter,taskm,testurl):
         threading.Thread.__init__(self)
@@ -23,6 +27,7 @@ class WorkerThread (threading.Thread):
                 log.loginfo("running in the counter:"+str(self.counter))
                 self.taskm(self.testurl)
                 self.counter -=1
+                increaseCounting()
         except Exception as e:
             log.loginfo("exception happened:"+e)
 
@@ -36,11 +41,29 @@ class WorkerThread (threading.Thread):
     #         print ("%s: %s" % (threadName, time.ctime(time.time())))
     #         counter -= 1
 
-
+class TestMonitorThread(threading.Thread):
+    def __init__(self,name,taskm):
+        threading.Thread.__init__(self)
+        self.name=name
+        self.taskm=taskm
+    def run(self):
+        
+        try:
+            log.loginfo("start test monitor")
+            while True:
+                showCounting()
+            
+        except Exception as e:
+            log.loginfo("exception happened:"+e)
+        finally:
+            log.loginfo("exit test monitor")
+            pass
 def runTestTask(taskn,taskm,testurl):
     # Create new threads
     maxThreads=taskn
     global interval
+    thread=TestMonitorThread("TestMonitor",showCounting)
+    thread.start()
     while maxThreads > 0:
         thread=WorkerThread(maxThreads,"TestWorker"+str(maxThreads),3,taskm,testurl)
         thread.start()
@@ -49,3 +72,16 @@ def runTestTask(taskn,taskm,testurl):
         maxThreads -=1
     
     log.loginfo ("Exiting main call")
+
+def increaseCounting():
+    global g_count
+    g_lock.acquire()
+    g_count+=1
+    g_lock.release
+
+def showCounting():
+    global g_next
+    global g_count
+    log.loginfo("current http request:"+str(g_count-g_next))
+    g_next=g_count
+    time.sleep(1)
