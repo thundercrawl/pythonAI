e72961a4b28e8708f9594b611e337e57cc838f1e
thundercrawl <brightpegion@gmail.com>
2019-04-16 09:50:55 +0800

    update

--- testmulti.py
+++ testmulti.py
@@ -7,19 +7,26 @@ import sys
 testurl = "http://www.baidu.com"
 
 def taskHttp(testurl):
-    starttime=time.time()*1000*1000
-    s = requests.session()
-    s.keep_alive = False
-    headers={
-        'Content-Type':'application/json',
-        'Connection':'close'
-    }
-    r= s.get(testurl,headers=headers,timeout=1)
-    log.loginfo(r.status_code)
-    endtime=time.time()*1000*1000
-    log.loginfo("time passed:"+str((endtime-starttime)/1000/1000)+" seconds")
-    r.close()
-    
+    r=None
+    try:
+        starttime=time.time()*1000*1000
+        s = requests.session()
+        s.keep_alive = False
+        headers={
+            'Content-Type':'application/json',
+            'Connection':'close'
+        }
+        r= s.get(testurl,headers=headers,timeout=1)
+        log.loginfo(r.status_code)
+        endtime=time.time()*1000*1000
+        log.loginfo("time passed:"+str((endtime-starttime)/1000/1000)+" seconds")
+        r.close()
+    except Exception as e:
+        log.loginfo("Exception e:"+e.__str__)
+    finally:
+        if(r!=None):
+            r.close()
+        log.loginfo("exit method taskhttp, clear resources")
 
 
 #main process
